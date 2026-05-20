4cd8fff1badae97dec03dc918aad3c2f21ada8ca
SINGHALOK28 <163043435+SINGHALOK28@users.noreply.github.com>
2026-05-20 15:41:38 +0530

    Time: 0 ms (100%), Space: 10.7 MB (7.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        transform(s.begin(),s.end(),s.begin(), ::tolower);
-        s=regex_replace(s,regex("[^a-z0-9]"),"");
-        int n = s.length();
-
-        for(int i=0;i<n/2;i++)
+        string ans = "";
+        for(auto it:s)
         {
-            if(s.at(i)!=s.at(n-1-i))
-            return false;
+            if(isalnum(it)){
+                ans += tolower(it);
+            }
         }
-        return true;
-    }
+    string res = ans;
+    reverse(ans.begin(),ans.end());
+
+    return ans==res?true:false;
+
+    //     transform(s.begin(),s.end(),s.begin(), ::tolower);
+    //     s=regex_replace(s,regex("[^a-z0-9]"),"");
+    //     int n = s.length();
+    //     for(int i=0;i<n/2;i++)
+    //     {
+    //         if(s.at(i)!=s.at(n-1-i))
+    //         return false;
+    //     }
+    //     return true;
+     }
 };
