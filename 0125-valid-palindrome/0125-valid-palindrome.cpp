ad6aed86e400cab2f91437ef3d7d8b92619bef94
Alok Singh <163043435+SINGHALOK28@users.noreply.github.com>
2026-07-22 23:12:18 +0530

    Time: 2 ms (36.12%), Space: 10 MB (54.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string ans = "";
-        for(auto it:s)
-        {
-            if(isalnum(it)){
-                ans += tolower(it);
-            }
-        }
-    string res = ans;
-    reverse(ans.begin(),ans.end());
+            int i=0;
+            int j=s.length()-1;
 
-    return ans==res?true:false;
+            while(i<j){
+                while(i<j && !isalnum(s[i]))i++;
+                while(i<j && !isalnum(s[j]))j--;
+
+                if(tolower(s[i])!=tolower(s[j]))
+                    return false;
+            i++;
+            j--;
+            }
+        return true;
+
+
+    //     string ans = "";
+    //     for(auto it:s)
+    //     {
+    //         if(isalnum(it)){
+    //             ans += tolower(it);
+    //         }
+    //     }
+    // string res = ans;
+    // reverse(ans.begin(),ans.end());
+
+    // return ans==res?true:false;
 
     //     transform(s.begin(),s.end(),s.begin(), ::tolower);
     //     s=regex_replace(s,regex("[^a-z0-9]"),"");
