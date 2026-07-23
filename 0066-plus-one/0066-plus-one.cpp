b098da478faa27a9d96ede9a4fd8c955b32af76e
Alok Singh <163043435+SINGHALOK28@users.noreply.github.com>
2026-07-23 11:44:32 +0530

    Time: 0 ms (100%), Space: 11.6 MB (28.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-    //     long long num = 0;
-    //     for (int x : digits) {
-    //     num = num * 10 + x;
-    // }
-    //  num = num+1;
-    // vector<int> result;
-    // string s = to_string(num);
-    // for (char c : s) {
-    //     result.push_back(c - '0');
-    // }
-    //     return result;
-
-
-    int n = digits.size();
-
+        int n = digits.size();
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
@@ -23,10 +9,7 @@ public:
             }
             digits[i] = 0;
         }
-
-        // agar sab 9 the
         digits.insert(digits.begin(), 1);
-        return digits;    
-
-        }
+        return digits;
+    }
 };
