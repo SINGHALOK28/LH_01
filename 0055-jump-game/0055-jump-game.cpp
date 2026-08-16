d930a6f91bc002ce00e768b85150a3545facc79b
Alok Singh <163043435+SINGHALOK28@users.noreply.github.com>
2026-08-16 11:01:03 +0530

    Time: 666 ms (5.18%), Space: 56 MB (5.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,33 +1,31 @@
-//GREEDY
-// class Solution {
-// public:
-//     bool canJump(vector<int>& nums) {
-//         int dum=nums[0];
-//         for(int i=1;i<nums.size();i++)
-//         {
-//             dum--;
-//             if(dum<0)
-//             {
-//                 return false;
-//             }
-//             dum = max(dum,nums[i]);
-//         }
-//     return dum>=0?true:false;
-//     }
-// };
-
-//2ND APPROACH 
-
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int finalpos=n-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]+i>=finalpos){
-                finalpos=i;
-            }
+    //  **/ recursion function /** 
+    //    bool solve(vector<int>& nums, int ind){  
+    //     if(ind==nums.size()-1) return true;
+    //     if(nums[ind]==0) return false;
+    //     int x=ind+nums[ind];
+    //     for(int i=ind+1;i<=x;i++){
+    //         if(i<nums.size() && solve(nums,i)) return true;
+    //     }
+
+    //     return false;
+    // }
+
+    bool solve2(vector<int>& nums, int ind,vector<int>& dp){
+        if(ind==nums.size()-1) return true;
+        if(nums[ind]==0) return false;
+        if(dp[ind]!=-1) return dp[ind];
+        int x=ind+nums[ind];
+        for(int i=ind+1;i<=x;i++){
+            if(i<nums.size() && solve2(nums, i, dp)) 
+                return dp[ind]= true;
         }
-        return finalpos==0?true:false;
+        return dp[ind]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        //return solve(nums,0); recursion
+        vector<int> dp(nums.size(), -1);
+        return solve2(nums, 0, dp);
     }
 };
