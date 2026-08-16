e3bc519910e3758c6ffdfdf9518ab5212a1f7790
Alok Singh <163043435+SINGHALOK28@users.noreply.github.com>
2026-08-16 15:05:24 +0530

    Time: 0 ms (100%), Space: 8 MB (89.16%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+// TLE de dega recursion
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n,int i=0, int j=0) {
+//         if(i>=m || j>=n) return 0;
+//         if(i==m-1 && j==n-1) return 1;
+//         return uniquePaths(m,n,i+1,j) + uniquePaths(m,n,i,j+1);
+//     }
+// };
+
+
+class Solution {
+public:
+    int dp[101][101];
+    int uniquePaths(int m, int n,int i=0, int j=0) {
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]) return dp[i][j];
+        return dp[i][j]=uniquePaths(m,n,i+1,j) + uniquePaths(m,n,i,j+1);
+    }
+};
