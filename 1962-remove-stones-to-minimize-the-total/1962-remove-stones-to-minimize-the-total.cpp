351c82b1da805a922cbfd1c8d3e64c731e3c5ab0
Alok Singh <163043435+SINGHALOK28@users.noreply.github.com>
2026-08-24 13:10:51 +0530

    Time: 285 ms (61.26%), Space: 108.8 MB (34.75%) - LeetHub

---
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int>pq;
+        for(int i=0;i<piles.size();i++){
+            pq.push(piles[i]);
+        }
+
+        while(k--){
+                int a = pq.top();
+                pq.pop();
+                a = a-(a/2);
+                pq.push(a);
+            }
+
+        int sum=0;
+        while(!pq.empty()){
+            sum = sum + pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
